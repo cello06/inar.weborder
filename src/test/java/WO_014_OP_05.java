80bd525825bbab7b1bb17820dff847d4bcb032c0
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2024-01-02 11:41:29 +0300

    new changes

--- src/test/java/WO_014_OP_05.java
+++ src/test/java/WO_014_OP_05.java
@@ -9,26 +9,28 @@ import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * 1- Open the URL. 2- Click "WebOrder" button on top bar. 3- Enter valid username "Inar"
- * and password "Academy". 4- Navigate to the order page. 5- Select "SportsEquipment" from
- * Product dropdown. 6- Enter "1" as quantity number. 7- Enter "10" as discount
- * percentage. 8- Click on the "Calculate" button. 9- Enter "Inar Academy" as Name. 10-
- * Enter "1100 Congress Ave" as Street. 11- Enter "Austin" as City. 12- Enter "TX" State.
- * 13- Enter "78701" as Zip Code(number). 14- Enter "493822019284565" as Card Number. 15-
- * Enter "09/26" Expire Date(mm/yy format). 16- Click "Process"" button. 17- Verify the
- * Card Type error message is displayed.
- */
-public class WO_009_OP_04 extends Hooks {
+public class WO_014_OP_05 extends Hooks {
 
+	/**
+	 * 1. Open the URL. 2. Click the "WebOrder" button on the top bar. 3. Enter valid
+	 * username "Inar" and password "Academy". 4. Navigate to the order page. 5. Select
+	 * "TechGadget" from Product dropdown. 6. Enter "10" as a quantity number. 7. Enter
+	 * "25" as a discount percentage. 8. Click on the "Calculate" button. 9. Enter
+	 * "Celalettin Aktürk" as Name. 10. Enter "Orhan Kemal" as Street. 11. Enter
+	 * "Altındağ" as City. 12. Enter "Ankara" State. 13. Enter "60800" as Zip
+	 * Code(number). 14. Select "Visa" as Card Type. 15. Enter "3938281746192845" as a
+	 * Card Number. 16. Enter "11/28" Expire Date(mm/yy format). 17. Click the "Process""
+	 * button. 18. Verify the confirmation message is not displayed. 19. Verify ‘Card
+	 * number is not valid’ error message is displayed.
+	 */
 	@BeforeEach
 	void setUp() {
-		System.out.println("WO_009_OP_04 --> Verifying Order Placement Without Card Type Test started");
+		System.out.println("WO_014_OP_05 --> Verifying y Order Placement with wrong card number Test started");
 
 	}
 
 	@Test
-	void testOrderPlacementWithoutCardType() throws InterruptedException {
+	void testOrderPlacementWithWrongCardNumber() throws InterruptedException {
 		// Click "WebOrder" button on top bar.
 		WebElement webOrderButton = driver.findElement(By.xpath("//a[@href='/weborder']"));
 		webOrderButton.click();
@@ -50,39 +52,39 @@ public class WO_009_OP_04 extends Hooks {
 		// Select "SportsEquipment" from Product dropdown.
 		WebElement productDropBowElement = driver.findElement(By.id("productSelect"));
 		Select select = new Select(productDropBowElement);
-		select.selectByVisibleText("SportsEquipment");
+		select.selectByVisibleText("TechGadget");
 
-		// Enter "1" as quantity number.
+		// Enter "10" as quantity number.
 		WebElement quantityElement = driver.findElement(By.id("quantityInput"));
-		quantityElement.sendKeys("1");
+		quantityElement.sendKeys("10");
 
-		// Enter "10" as discount percentage.
+		// Enter "25" as discount percentage.
 		WebElement discountElement = driver.findElement(By.id("discountInput"));
-		discountElement.sendKeys("10");
+		discountElement.sendKeys("25");
 
 		// Click on the "Calculate" button.
 		WebElement calculateButtonElement = driver.findElement(By.xpath("//button[contains(text(),'Calculate')]"));
 		calculateButtonElement.click();
 
-		// Enter "Inar Academy" as Name.
+		// Enter "Celalettin Aktürk" as Name.
 		WebElement nameElement = driver.findElement(By.id("name"));
-		nameElement.sendKeys("Inar Academy");
+		nameElement.sendKeys("Celalettin Aktürk");
 
-		// Enter "1100 Congress Ave" as Street.
+		// Enter "Orhan Kemal" as Street.
 		WebElement streetElement = driver.findElement(By.id("street"));
-		streetElement.sendKeys("1100 Congress Ave");
+		streetElement.sendKeys("Orhan Kemal");
 
-		// Enter "Austin" as City.
+		// Enter "Altındağ" as City.
 		WebElement cityElement = driver.findElement(By.id("city"));
-		cityElement.sendKeys("Austin");
+		cityElement.sendKeys("Altındağ");
 
-		// Enter "TX" State.
+		// Enter "Ankara" State.
 		WebElement stateElement = driver.findElement(By.id("state"));
-		stateElement.sendKeys("TX");
+		stateElement.sendKeys("Ankara");
 
-		// Enter "78701" as Zip Code(number).
+		// Enter "60800" as Zip Code(number).
 		WebElement zipcodeElement = driver.findElement(By.id("zip"));
-		zipcodeElement.sendKeys("78701");
+		zipcodeElement.sendKeys("60800");
 
 		// We need to scroll down to choose card type
 
@@ -90,36 +92,39 @@ public class WO_009_OP_04 extends Hooks {
 		js.executeScript("window.scroll(0,1200)");
 		Thread.sleep(1000);
 
-		// We intentionally left blank 'Card Type'
+		// Select "Visa" as Card Type.
+		WebElement cardType = driver.findElement(By.id("visa"));
+		cardType.click();
 
-		// Enter "493822019284565" as Card Number.
+		// Enter "3938281746192845" as Card Number.
 		WebElement cardNumberElement = driver.findElement(By.id("cardNumber"));
-		cardNumberElement.sendKeys("493822019284565");
+		cardNumberElement.sendKeys("3938281746192845");
 
-		// Enter "09/26" Expire Date(mm/yy format).
+		// Enter "11/28" Expire Date(mm/yy format).
 		WebElement expDateElement = driver.findElement(By.id("expiryDate"));
-		expDateElement.sendKeys("09/26");
+		expDateElement.sendKeys("11/28");
 
 		// Click "Process"" button.
 		WebElement processButtonElement = driver.findElement(By.xpath("//div/button[@type='submit']"));
 		processButtonElement.click();
 
 		/**
-		 * We expect to see 'Card type cannot be empty' error message And there must not
-		 * be 'Product successfully added' message
+		 * We expect to see 'Card number is not valid' error message And there must not be
+		 * 'Product successfully added' message
 		 */
 
 		boolean firstCondition = false;
 
 		try {
 			WebElement cardTypeCannotBeEmptyErrorMessage = driver
-				.findElement(By.xpath("//em[contains(text(),'Card type cannot be empty')]"));
+				.findElement(By.xpath("//em[contains(text(),'Card number is not valid')]"));
 			if (cardTypeCannotBeEmptyErrorMessage.isDisplayed()) {
 				firstCondition = true;
 			}
 		}
 		catch (NoSuchElementException e) {
-			throw new NoSuchElementException("'Card type cannot be empty' error message is not found!");
+			throw new NoSuchElementException(
+					"'Card number is not valid' error message is not found! Although invalid card number input");
 		}
 
 		boolean secondCondition = false;
@@ -128,7 +133,7 @@ public class WO_009_OP_04 extends Hooks {
 			WebElement newOrderSuccessfullyAddedMessageElement = driver.findElement(By.cssSelector("div[role=alert]"));
 			if (newOrderSuccessfullyAddedMessageElement.isDisplayed()) {
 				System.out.println("there must not be 'Product successfully added' message\n"
-						+ "-ERROR- Although card type did not selected the order addition message displayed");
+						+ "-ERROR- Although card type is not valid for 'visa' card type the order addition message displayed");
 			}
 		}
 		catch (NoSuchElementException e) {
@@ -137,22 +142,21 @@ public class WO_009_OP_04 extends Hooks {
 		}
 
 		assertTrue(firstCondition && secondCondition,
-				"Although card type selection is not happen, error message did not display!");
+				"Although card number is not valid for card type, error message did not display!");
 
 		System.out.println("In This Test Case : \n"
-				+ "By not entering Card Type we test Order Placement under two sections:\n"
+				+ "By entering invalid card number we test Order Placement under two sections:\n"
 				+ "1-'New order has been successfully added' message exhibition \n"
-				+ "2- 'Card type cannot be empty' message exhibition are tested\n"
+				+ "2- 'Card number is not valid' message exhibition are tested\n"
 				+ "Expected Scenario = 1-'New order has been successfully added' must not be displayed\n"
-				+ "                    2-'Card type cannot be empty' message must be displayed\n"
+				+ "                    2-'Card number is not valid' message must be displayed\n"
 				+ "Actual Scenario = 1- As expected 'New order has been successfully added' message is not displayed\n"
-				+ "                  2- As expected 'Card type cannot be empty' message is displayed");
-
+				+ "                  2- As expected 'Card number is not valid' message is displayed");
 	}
 
 	@AfterEach
 	void tearDown() {
-		System.out.println("WO_009_OP_04 --> Verifying Order Placement Without Card Type Test finished");
+		System.out.println("WO_014_OP_05 --> Verifying y Order Placement with wrong card number Test finished");
 		System.out.println("###########################################################");
 	}
 
